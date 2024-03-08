b36d8b5f13933b32d51c17ed1f464624ef2a046a
Nandan Kumar <142118827+NandanKumar1475@users.noreply.github.com>
2024-03-08 15:51:32 +0530

    Add files via upload
    updated routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import {createUser} from '../controlers/todoControllers.js';
+import { getUser,getUserbyId } from '../controlers/getTodoControllers.js';
+import {updateTodo} from '../controlers/updateTodControllers.js';
+import {deleteuser} from '../controlers/deleteTodoController.js'
 const router = express.Router();
 
 
 router.post('/User',createUser);
+router.get('/getuser',getUser);
+router.get('/getuser/:id',getUserbyId);
+router.put('/updateuser/:id',updateTodo);
+router.delete('/deleteuser/:id',deleteuser)
 
 export default router;
 
